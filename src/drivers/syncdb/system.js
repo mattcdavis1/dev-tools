bb8a90ab30b7e88f7272ad5cf71596b98192b2a6
Matt Davis <matt.davis@union.co>
2018-06-09 16:32:09 -0400

    working so far...

--- src/drivers/syncdb/system.js
+++ src/drivers/syncdb/system.js
@@ -1,8 +1,9 @@
 // global requires
-const { createWriteStream } = require('fs');
+const { createWriteStream, mkDirSync, existsSync } = require('fs');
 const { execSync, spawnSync } = require('child_process');
 const chalk = require('chalk');
 const dotenv = require('dotenv');
+const path = require('path');
 
 // vars
 const SQL_DUMP_FILE_NAME = 'database_dump.sql';
@@ -28,9 +29,11 @@ class SystemSsh {
 
     execute() {
         console.log(chalk.yellow('Parsing Remote Env File...'));
+        const local = this.local;
+        const remote = this.remote;
 
         // get remote env
-        const { stdout } = this.execRemote(`cat ${this.remote.pathDotEnv}`, { stdio: 'pipe' });
+        const { stdout } = this.execRemote(`cat ${remote.pathDotEnv}`, { stdio: 'pipe' });
         const {
             DB_DATABASE,
             DB_PASSWORD,
@@ -38,34 +41,53 @@ class SystemSsh {
             DB_SERVER,
             DB_USER,
             MYSQL_DUMP_PATH = '/usr/bin/mysqldump',
-            SSH_PORT,
         } = dotenv.parse(stdout.toString('utf8'));
 
         // execute remote mysqldump
-
-        const remoteSqlDumpPath = `${this.remote.backupDirectory}/${SQL_DUMP_FILE_NAME}`;
-        const remoteSqlDumpZipPath = `${this.remote.backupDirectory}/${SQL_DUMP_FILE_NAME_ZIP}`;
+        const remoteSqlDumpPath = path.join(remote.pathBackupDirectory, SQL_DUMP_FILE_NAME);
+        const remoteSqlDumpZipPath = path.join(remote.pathBackupDirectory, SQL_DUMP_FILE_NAME_ZIP);
 
         const dumpSqlCommand = `${MYSQL_DUMP_PATH} -h ${DB_SERVER} -u ${DB_USER} -p"${DB_PASSWORD}" -P ${DB_PORT} ${DB_DATABASE} > ${remoteSqlDumpPath}`;
         const zipSqlCommand = `zip -j ${remoteSqlDumpZipPath} ${remoteSqlDumpPath}`;
-        const rmSqlCommand = `rm ${remoteSqlDumpPath}`;
+        const rmSqlCommand = `rm ${remoteSqlDumpPath}; rm ${remoteSqlDumpZipPath}`;
 
         console.log(chalk.yellow('Beginning Remote Dump...'));
 
         this.execRemote(`${dumpSqlCommand}; ${zipSqlCommand}; ${rmSqlCommand}`);
 
-        const localSqlDumpPath = `${LOCAL_BACKUP_DIRECTORY}/${SQL_DUMP_FILE_NAME}`;
-        const localSqlDumpZipPath = `${LOCAL_BACKUP_DIRECTORY}/${SQL_DUMP_FILE_NAME_ZIP}`;
+        console.log(chalk.green('Remote Dump Complete'));
 
-        const downloadZipCommand = `scp ${this.remote.host}:${remoteSqlDumpZipPath} ${localSqlDumpZipPath}`;
+        // download remote sql dump zip file
+        console.log(chalk.yellow('Downloading Remote SQL Dump...'));
 
+        const backupDirectoryPath = path.join(this.local.projectPath, LOCAL_BACKUP_DIRECTORY);
+
+        if (!existsSync(backupDirectoryPath)) {
+            this.execLocal(`mkdir -p ${backupDirectoryPath}`);
+        }
+
+        const localSqlDumpPath = path.join(backupDirectoryPath, SQL_DUMP_FILE_NAME);
+        const localSqlDumpZipPath = path.join(backupDirectoryPath, SQL_DUMP_FILE_NAME_ZIP);
+
+        const downloadZipCommand = `scp ${remote.host}:${remoteSqlDumpZipPath} ${localSqlDumpZipPath}`;
         this.execLocal(downloadZipCommand);
+
+        console.log(chalk.green('SQL Zip File Download Complete'));
+
+        const unzipZipCommand = `unzip ${localSqlDumpZipPath} -do ${backupDirectoryPath}`;
+        const importMysqlCommand = `mysql -u ${local.dbUser} -h ${local.dbServer} -P ${local.dbPort} --password="${local.dbPassword}" ${local.dbDatabase} < ${localSqlDumpPath}`;
+        const deleteLocalFilesCommand = `rm ${localSqlDumpPath}; rm ${localSqlDumpZipPath}`;
+
+        this.execLocal(`${unzipZipCommand}; ${importMysqlCommand}; ${deleteLocalFilesCommand}`);
+
+        console.log(chalk.green('SyncDB Complete'));
     }
 
-    execLocal(command, { stdio = 'inherit' }) {
+
+    execLocal(command, { stdio = 'inherit' } = {}) {
         const localCommand = spawnSync(command, {
             shell: true,
-            stdio: 'inherit',
+            stdio,
         });
 
         if (localCommand.error) {
@@ -76,10 +98,10 @@ class SystemSsh {
         return localCommand;
     }
 
-    execRemote(command, { stdio = 'inherit' }) {
+    execRemote(command, { stdio = 'inherit' } = {}) {
         const host = this.remote.username ? `${this.remote.username}@${this.remote.host}` : this.remote.host;
 
-        const sshCommand = spawnSync(`ssh ${host} -p ${this.remote.port} ${command}`, {
+        const sshCommand = spawnSync(`ssh ${host} -p ${this.remote.port} '${command}'`, {
             shell: true,
             stdio,
         });
